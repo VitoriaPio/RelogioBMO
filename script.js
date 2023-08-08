5334c7b9cd1f942803e407290cd0600e83f22c0b
VitoriaPio <pio.vitoria156@gmail.com>
2023-08-08 15:27:53 -0300

    Relógio BMO

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var msg = document.getElementById("msg");
 var data = new Date();
 var hora = data.getHours();
 var min = data.getMinutes();
-var seg = data.getUTCSeconds();
+var seg = data.getSeconds();
 
 msg.innerHTML = `${hora}:${min}:${seg}`
 
